d2bbd37052b17b03e7b36f7bdf1d1f266739903c
Stuart Fraser <stuartrfraser@live.co.uk>
2014-11-27 16:34:53 +0000

    Cleaned up pins scafold and there design

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -22,13 +22,19 @@ class PinsController < ApplicationController
 
   def create
     @pin = Pin.new(pin_params)
-    @pin.save
-    respond_with(@pin)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
   def update
-    @pin.update(pin_params)
-    respond_with(@pin)
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
