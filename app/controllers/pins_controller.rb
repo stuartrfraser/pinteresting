1fe82a973bfaf9a03b102ea2883f1102e6d49ece
Stuart Fraser <stuartrfraser@live.co.uk>
2014-12-04 11:18:30 +0000

    Added paperclip gem for images

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   respond_to :html
 
@@ -54,6 +54,6 @@ class PinsController < ApplicationController
     end
 
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
